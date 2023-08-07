a258cd86642f270243094be024bbf0ff49890836
aniket132123 <anisingh27@yahoo.com>
2023-08-07 13:43:17 -0700

    text appears in new activity
    - slightly cleaner looking
    - website is down so isbn search is currently also down
    - add a new feature to search by author and book name

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -57,27 +57,20 @@ import org.jsoup.select.Elements;
 
 @ExperimentalGetImage public class MainActivity extends AppCompatActivity implements ImageAnalysis.Analyzer{
     private PreviewView previewView;
-    private TextView otherTextView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ImageCapture imageCapture;
     private BarcodeScannerOptions options;
-    private ExecutorService backgroundSearch;
-    private static String bookTitle;
-    private static String bookAuthor;
     private String barcodeValue;
-    private ArrayList<String> reviewCollection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_camera);
-        otherTextView = findViewById(R.id.otherTextView);
         previewView = findViewById(R.id.previewView);
         previewView.setPreferredImplementationMode(PreviewView.ImplementationMode.SURFACE_VIEW);
         Button takePictureButton = findViewById(R.id.button_capture);
         Button searchButton = findViewById(R.id.button_search);
-        ExecutorService backgroundSearch = Executors.newSingleThreadExecutor();
-
 
         options =
                 new BarcodeScannerOptions.Builder()
@@ -102,26 +95,15 @@ import org.jsoup.select.Elements;
             @Override
             public void onClick(View v) {
                 enableCamera();
-
             }
         });
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                backgroundSearch.execute(() -> {
-                    runOnUiThread(() -> Toast.makeText(MainActivity.this, "Search Initiated", Toast.LENGTH_SHORT).show());
-                    getInfoFromISBN(barcodeValue);
-                    storyGraphBookResults();
-                    barnesAndNobleBookResults();
-                    kirkusBookResults();
-                    googleBooksResults();
-//                    otherTextView.setText(reviewCollection.toString());
-                    Intent searchIntent = new Intent(MainActivity.this, TextDisplay.class);
-                    startActivity(searchIntent);
-                    runOnUiThread(() -> Toast.makeText(MainActivity.this, "Search Complete", Toast.LENGTH_SHORT).show());
-                });
-
+                Intent searchIntent = new Intent(MainActivity.this, TextDisplay.class);
+                searchIntent.putExtra("KEY_SENDER", barcodeValue);
+                startActivity(searchIntent);
             }
         });
     }
@@ -210,126 +192,9 @@ import org.jsoup.select.Elements;
                         }
                         else barcodeValue = "none";
 
-
                     })
                     .addOnFailureListener(
                             e -> Toast.makeText(MainActivity.this, "Process Failed", Toast.LENGTH_SHORT).show());
             }
         }
-
-    //grabs information from an ISBN number
-    private void getInfoFromISBN(String barcodeString){
-        reviewCollection = new ArrayList<>();
-        Document doc;
-        try {
-            doc = Jsoup.connect("https://www.isbnsearcher.com/books/" + barcodeString)
-                    .timeout(5000)
-                    .get();
-            Elements title = doc.select("h1");
-            Elements author = doc.getElementsByAttributeValueContaining("href", "author");
-            bookTitle = title.text();
-            bookAuthor = author.text();
-
-        } catch (IOException ignore){}
-    }
-
-    //primarily here to grab customer reviews
-    private void storyGraphBookResults(){
-        Document doc;
-        try {
-            String modifiedBookTitle = bookTitle.replace(" ", "%20");
-            String modifiedAuthorName = bookAuthor.replace(" ", "%20");
-
-            doc = Jsoup.connect("https://app.thestorygraph.com/browse?search_term=" + modifiedBookTitle + modifiedAuthorName)
-                    .timeout(10000)
-                    .get();
-
-            Elements links = doc.select("a[href]");
-            String bookCode = links.get(14).attr("href");
-            doc = Jsoup.connect("https://app.thestorygraph.com/" + bookCode.replace("/books", "book_reviews") + "?written_explanations_only=true")
-                    .timeout(10000)
-                    .get();
-            Elements reviews = doc.getElementsByClass("trix-content review-explanation");
-            for(Element review : reviews){
-                reviewCollection.add(review.text());
-            }
-        } catch (Exception ignored) {}
-    }
-    //primarily here to grab editorial reviews
-    private void barnesAndNobleBookResults(){
-        Document doc;
-        try {
-            String modifiedBookTitle = bookTitle.replace(" ", "+");
-            String modifiedAuthorName = bookAuthor.replace(" ", "+");
-
-
-            doc = Jsoup.connect("https://www.google.com/search?q=" + modifiedBookTitle + "+" + modifiedAuthorName + "+barnes+and+noble+book+review")
-                    .timeout(10000)
-                    .get();
-            Elements links = doc.getElementsByAttributeValueContaining("href", "https://www.barnesandnoble.com/");
-
-            doc = Jsoup.connect(links.get(0).attr("href"))
-                    .timeout(10000)
-                    .get();
-
-            //checks for a block quote containing an editorial review
-            Elements editorialReview = doc.getElementsByTag("blockquote");
-            reviewCollection.add(editorialReview.text());
-
-            // //searches for a generic overview
-            Elements reviewsSection = doc.getElementsByClass( "overview-cntnt");
-            for(Element review : reviewsSection){
-                reviewCollection.add(review.text());
-            }
-        }
-        catch (Exception ignored) {}
-    }
-
-    private void kirkusBookResults(){
-        Document doc;
-        try {
-            String modifiedBookTitle = bookTitle.replace(" ", "-").toLowerCase();
-            String modifiedAuthorName = bookAuthor.replace(" ", "-").toLowerCase();
-            doc = Jsoup.connect("https://www.kirkusreviews.com/book-reviews/"+ modifiedAuthorName + "/" + modifiedBookTitle + "/")
-                    .timeout(10000)
-                    .get();
-            Elements reviewStuff = doc.getElementsByClass("first-alphabet book-content text-left");
-
-            reviewCollection.add(reviewStuff.first().text());
-        } catch (Exception ignored) {}
-    }
-
-    private void googleBooksResults(){
-        Document doc;
-        try {
-            String modifiedBookTitle = bookTitle.replace(" ", "-").toLowerCase();
-            String modifiedAuthorName = bookAuthor.replace(" ", "-").toLowerCase();
-            doc = Jsoup.connect("https://www.google.com/search?q=" + modifiedBookTitle + "+" + modifiedAuthorName + "+google+books+book+review")
-                    .timeout(10000)
-                    .get();
-
-            Elements links = doc.getElementsByAttributeValueContaining("href", "https://books.google.com");
-
-            doc = Jsoup.connect(links.first().attr("href"))
-                    .timeout(10000)
-                    .get();
-            Elements reviewLink = doc.getElementsByAttributeValueContaining("href", "=reviews");
-
-            doc = Jsoup.connect(reviewLink.attr("href"))
-                    .timeout(10000)
-                    .get();
-            Elements reviews = doc.getElementsByClass("single-review");
-            for (Element review : reviews) {
-                String modifiedReview = review.text().replace("Review:", "");
-                reviewCollection.add(modifiedReview.replace("Read full review", ""));
-            }
-
-        } catch (Exception ignored) {}
-    }
-
-    public String getReviews() {
-        if (reviewCollection != null){
-            return reviewCollection.toString();
-        } else return "guh";
-    }
 }
--- app/src/main/java/com/example/cameraapp/TextDisplay.java
+++ app/src/main/java/com/example/cameraapp/TextDisplay.java
@@ -4,24 +4,159 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.camera.core.ExperimentalGetImage;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @ExperimentalGetImage public class TextDisplay extends MainActivity {
     private TextView textView;
-
+    private static String bookTitle;
+    private static String bookAuthor;
+    private ArrayList<String> reviewCollection;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_display);
         textView = findViewById(R.id.textView);
         Button backButton = findViewById(R.id.back_button);
-//        if (!reviewCollection.isEmpty())
-            textView.setText(getReviews());
-//        else textView.setText("");
+        ExecutorService backgroundSearch = Executors.newSingleThreadExecutor();
+        backgroundSearch.execute(() -> {
+            Intent receiverIntent = getIntent();
+            String receivedValue = receiverIntent.getStringExtra("KEY_SENDER");
+            getInfoFromISBN(receivedValue);
+            storyGraphBookResults();
+            barnesAndNobleBookResults();
+            kirkusBookResults();
+            googleBooksResults();
+            textView.setText(reviewCollection.toString());
+        });
+
         backButton.setOnClickListener(v -> {
             Intent returnIntent = new Intent(TextDisplay.this, MainActivity.class);
             startActivity(returnIntent);
         });
+
+    }
+    private void getInfoFromISBN(String barcodeString){
+        reviewCollection = new ArrayList<>();
+        Document doc;
+        try {
+            doc = Jsoup.connect("https://www.isbnsearcher.com/books/" + barcodeString)
+                    .timeout(5000)
+                    .get();
+            Elements title = doc.select("h1");
+            Elements author = doc.getElementsByAttributeValueContaining("href", "author");
+            bookTitle = title.text();
+            bookAuthor = author.text();
+
+        } catch (IOException ignore){
+            reviewCollection.add("Book could not be found.");
+        }
+    }
+
+    //primarily here to grab customer reviews
+    private void storyGraphBookResults(){
+        Document doc;
+        try {
+            String modifiedBookTitle = bookTitle.replace(" ", "%20");
+            String modifiedAuthorName = bookAuthor.replace(" ", "%20");
+
+            doc = Jsoup.connect("https://app.thestorygraph.com/browse?search_term=" + modifiedBookTitle + modifiedAuthorName)
+                    .timeout(10000)
+                    .get();
+
+            Elements links = doc.select("a[href]");
+            String bookCode = links.get(14).attr("href");
+            doc = Jsoup.connect("https://app.thestorygraph.com/" + bookCode.replace("/books", "book_reviews") + "?written_explanations_only=true")
+                    .timeout(10000)
+                    .get();
+            Elements reviews = doc.getElementsByClass("trix-content review-explanation");
+            for(Element review : reviews){
+                reviewCollection.add(review.text());
+            }
+
+        } catch (Exception ignored) {}
+    }
+    //primarily here to grab editorial reviews
+    private void barnesAndNobleBookResults(){
+        Document doc;
+        try {
+            String modifiedBookTitle = bookTitle.replace(" ", "+");
+            String modifiedAuthorName = bookAuthor.replace(" ", "+");
+
+
+            doc = Jsoup.connect("https://www.google.com/search?q=" + modifiedBookTitle + "+" + modifiedAuthorName + "+barnes+and+noble+book+review")
+                    .timeout(10000)
+                    .get();
+            Elements links = doc.getElementsByAttributeValueContaining("href", "https://www.barnesandnoble.com/");
+
+            doc = Jsoup.connect(links.get(0).attr("href"))
+                    .timeout(10000)
+                    .get();
+
+            //checks for a block quote containing an editorial review
+            Elements editorialReview = doc.getElementsByTag("blockquote");
+            reviewCollection.add(editorialReview.text());
+
+            // //searches for a generic overview
+            Elements reviewsSection = doc.getElementsByClass( "overview-cntnt");
+            for(Element review : reviewsSection){
+                reviewCollection.add(review.text());
+            }
+        }
+        catch (Exception ignored) {}
+    }
+
+    private void kirkusBookResults(){
+        Document doc;
+        try {
+            String modifiedBookTitle = bookTitle.replace(" ", "-").toLowerCase();
+            String modifiedAuthorName = bookAuthor.replace(" ", "-").toLowerCase();
+            doc = Jsoup.connect("https://www.kirkusreviews.com/book-reviews/"+ modifiedAuthorName + "/" + modifiedBookTitle + "/")
+                    .timeout(10000)
+                    .get();
+            Elements reviewStuff = doc.getElementsByClass("first-alphabet book-content text-left");
+
+            reviewCollection.add(reviewStuff.first().text());
+        } catch (Exception ignored) {}
+    }
+
+    private void googleBooksResults(){
+        Document doc;
+        try {
+            String modifiedBookTitle = bookTitle.replace(" ", "-").toLowerCase();
+            String modifiedAuthorName = bookAuthor.replace(" ", "-").toLowerCase();
+            doc = Jsoup.connect("https://www.google.com/search?q=" + modifiedBookTitle + "+" + modifiedAuthorName + "+google+books+book+review")
+                    .timeout(10000)
+                    .get();
+
+            Elements links = doc.getElementsByAttributeValueContaining("href", "https://books.google.com");
+
+            doc = Jsoup.connect(links.first().attr("href"))
+                    .timeout(10000)
+                    .get();
+            Elements reviewLink = doc.getElementsByAttributeValueContaining("href", "=reviews");
+
+            doc = Jsoup.connect(reviewLink.attr("href"))
+                    .timeout(10000)
+                    .get();
+            Elements reviews = doc.getElementsByClass("single-review");
+            for (Element review : reviews) {
+                String modifiedReview = review.text().replace("Review:", "");
+                reviewCollection.add(modifiedReview.replace("Read full review", ""));
+            }
+
+
+        } catch (Exception ignored) {}
     }
 }
